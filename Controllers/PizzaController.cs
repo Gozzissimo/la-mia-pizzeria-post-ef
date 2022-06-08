5432be236af2b58fe58eb1d33bc97669ac79cfea
Gozzissimo <fantini.nico@gmail.com>
2022-06-08 14:22:11 +0200

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using la_mia_pizzeria_static.Models;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        static Pizzeria pizzeria = new Pizzeria("Bella Napoli");
+
+        public IActionResult Index()
+        {
+            return View(pizzeria.listaPizze);
+        }
+
+        public IActionResult ShowPizza(int id)
+        {
+            Pizza? pizzaCercata = pizzeria.listaPizze.Find(item => item.Id == id);
+            if (pizzaCercata == null)
+            {
+                ViewData["Titolo"] = "Pizza Not Found!";
+                return View("PizzaNotFound");
+            }
+            else
+            {
+                ViewData["nomePizzeria"] = pizzeria.Nome;
+                return View(pizzaCercata);
+            }
+        }
+
+        //CREATE
+        public IActionResult CreatePizza()
+        {
+            Pizza nuovaPizza = new Pizza();
+            return View(nuovaPizza);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ValidatePizza(Pizza nuovaPizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("CreatePizza", nuovaPizza);
+            }
+
+            if (nuovaPizza.formFile != null)
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\img");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                FileInfo newFileInfo = new FileInfo(nuovaPizza.formFile.FileName);
+                string fileName = String.Format("{0}{1}", nuovaPizza.Name, newFileInfo.Extension);
+                string FullPathName = Path.Combine(path, fileName);
+                using (FileStream stream = new FileStream(FullPathName, FileMode.Create))
+                {
+                    nuovaPizza.formFile.CopyTo(stream);
+                    stream.Close();
+                }
+                nuovaPizza.ImgPath = String.Format("{0}", fileName);
+            }
+            pizzeria.addPizza(nuovaPizza);
+            return View("ShowPizza", nuovaPizza);
+        }
+
+        //VIEW DELLA UPDATE
+        [HttpGet]
+        public IActionResult UpdatePizza(int id)
+        {
+            Pizza? pizzaDaModificare = pizzeria.listaPizze.Find(item => item.Id == id);
+            if (pizzaDaModificare == null)
+            {
+                ViewData["Titolo"] = "Pizza Not Found!";
+                return View("PizzaNotFound");
+            }
+            else
+            {
+                ViewData["nomePizzeria"] = pizzeria.Nome;
+                return View(pizzaDaModificare);
+            }
+        }
+
+        //METODO PER UPDATARE LE PIZZE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, Pizza modello)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("UpdatePizza", modello);
+            }
+
+            Pizza? pizzaDaModificare = pizzeria.listaPizze.Find(item => item.Id == id);
+
+            if (pizzaDaModificare != null)
+            {
+                //aggiorniamo i campi con i nuovi valori
+                pizzaDaModificare.Name = modello.Name;
+                pizzaDaModificare.Description = modello.Description;
+                pizzaDaModificare.Price = modello.Price;
+            }
+            else
+            {
+                ViewData["Titolo"] = "Pizza Not Found!";
+                return View("PizzaNotFound");
+            }
+
+            return RedirectToAction("ShowPizza");
+        }
+
+
+        //DELETE
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Pizza? pizzaDaRimuovere = pizzeria.listaPizze.Find(item => item.Id == id);
+
+            pizzeria.listaPizze.Remove(pizzaDaRimuovere);
+            return RedirectToAction("Index");
+        }
+    }
+}
+ 
---
+++ Models/Pizzeria.cs
@@ -0,0 +1,22 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizzeria
+    {
+        public string Nome;
+        public List<Pizza> listaPizze;
+        public Pizzeria(string nome)
+        {
+            this.Nome = nome;
+            listaPizze = new List<Pizza>();
+            this.addPizza(new Pizza(0, "Margherita", "Pomodori, Mozzarella, Basilico, Olio EVO", "margherita.jpg", 10.50));
+            this.addPizza(new Pizza(1, "Diavola", "Pomodori, Mozzarella, Salame Piccante, Olio EVO Piccante", "diavola.jpg", 7.80));
+            this.addPizza(new Pizza(2, "Capricciosa", "Pomodori, Mozzarella, Prosciutto Cotto, Funghi, Olive, Carciofini", "capricciosa.jpg", 15.00));
+            this.addPizza(new Pizza(3, "Americana", "Pomodori, Mozzarella, Wurstel, Patatine Fritte", "americana.jpg", 13.05));
+            this.addPizza(new Pizza(4, "Salsiccia e Friarielli", "Mozzarella, Salsiccia, Friarielli", "salsiccia-friarielli.jpg", 13.05));
+        }
+        public void addPizza(Pizza pizza)
+        {
+            listaPizze.Add(pizza);
+        }
+    }
+}
